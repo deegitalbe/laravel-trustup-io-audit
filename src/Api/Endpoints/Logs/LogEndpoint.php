68353106fa4b00f278111dd9d2a1fd3aedc01ae4
Pierre Wasilewski <pierrewasilewski@MacBook-Air-de-Pierre.local>
2023-02-03 10:47:14 +0100

    :sparkles: Adding index enpoint.
    git push

--- src/Api/Endpoints/Logs/LogEndpoint.php
+++ src/Api/Endpoints/Logs/LogEndpoint.php
@@ -17,24 +17,25 @@ class LogEndpoint implements LogEndpointContract
 
     protected ClientContract $client;
 
-    public function __construct(ClientContract $client, LogCredential $credential)
+    public function __construct(ClientContract $client, LogCredential $credential, protected RequestContract $request)
     {
         $this->client = $client->setCredential($credential);
+        $this->request = $request;
     }
 
     public function store(StoreLogRequestContract $storeRequest): StoreLogResponseContract
     {
-        /** @var RequestContract */
-        $request = app()->make(RequestContract::class);
-        $request->setVerb("POST")->setUrl("logs")->addData($storeRequest->toArray());
-        $response = $this->client->try($request, "Cannot store log");
+
+        $this->request->setVerb("POST")->setUrl("logs")->addData($storeRequest->toArray());
+        $response = $this->client->try($this->request, "Cannot store log");
         /** @var StoreLogResponseContract */
         return app()->make(StoreLogResponseContract::class)->setResponse($response->response());
     }
 
     public function index(IndexLogRequestContract $request): IndexLogResponseContract
     {
-
+        $this->request->setVerb("GET")->setUrl("logs");
+        $response = $this->client->try($this->request, "Cannot get logs");
         /** @var IndexLogResponseContract */
         return app()->make(IndexLogResponseContract::class)->setResponse($response->response());
     }
